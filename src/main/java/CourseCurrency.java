8fa605a806b1568976c60b5fb5a7ec5bf045ab28
korvalanchik <korvalan@yahoo.com>
2023-10-22 00:29:05 +0300

    Ver. 2

--- src/main/java/CourseCurrency.java
+++ src/main/java/CourseCurrency.java
@@ -8,11 +8,9 @@ import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CourseCurrency {
 //    private static final Gson GSON = new Gson();
-    private static final String URL_COURSE_NBU = "https://bank.gov.ua/NBU_Exchange/exchange?json";
     private static final String BASE_URL_COURSE_NBU = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json&date=";
     public ArrayList<Currency> getNBU(String currentdateYYYYMMDD) throws IOException {
         URL url = new URL(BASE_URL_COURSE_NBU + currentdateYYYYMMDD);
@@ -35,7 +33,7 @@ public class CourseCurrency {
         }
         StringReader reader = new StringReader(String.valueOf(response));
         ObjectMapper mapper = new ObjectMapper();
-        ArrayList<Currency> currencyList = mapper.readValue(reader, new TypeReference<ArrayList<Currency>>(){});
+        ArrayList<Currency> currencyList = mapper.readValue(reader, new TypeReference<>() {});
         //        List<Currency> currencyList = GSON.fromJson(reader, new TypeToken<List<Currency>>(){}.getType());
 //        System.out.println(currencyList);
         return currencyList;
--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Demo {
     public static void main(String[] args) throws IOException {
