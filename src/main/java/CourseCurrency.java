dc4ba73bfeeb499043da650e61ca059336c58a2c
korvalanchik <korvalan@yahoo.com>
2023-10-22 23:20:23 +0300

    Ver. 3.0.1

--- src/main/java/CourseCurrency.java
+++ src/main/java/CourseCurrency.java
@@ -9,8 +9,19 @@ import java.net.URL;
 
 public class CourseCurrency {
     private static final String BASE_URL_COURSE_NBU = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json&date=";
-    public Currency[] getNBU(String currentdateYYYYMMDD) throws IOException {
-        URL url = new URL(BASE_URL_COURSE_NBU + currentdateYYYYMMDD);
+    private static final String BASE_URL_COURSE_PRIVAT = "https://api.privatbank.ua/p24api/exchange_rates?json&date=";
+    public Currency[] getNBU(String currentdateDDpMMpYYYY) throws IOException {
+        String urlString, currentdateYYYYMMDD;
+        if(currentdateDDpMMpYYYY != null) {
+            // convert DDMMYYYY to YYYYMMDD
+            //....
+            //....
+            urlString = BASE_URL_COURSE_NBU + "&date=" + "20231023";
+        } else {
+            urlString = BASE_URL_COURSE_NBU;
+        }
+        URL url = new URL(urlString);
+
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Content-Type", "application/json");
@@ -31,5 +42,16 @@ public class CourseCurrency {
         StringReader reader = new StringReader(String.valueOf(response));
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(reader, Currency[].class);
+    }
+
+    /*
+baseCurrency                Базова валюта
+currency	                Валюта угоди
+saleRateNB/purchaseRateNB	Курс продажу НБУ
+saleRate	                Курс продажу ПриватБанку
+purchaseRate	            Курс купівлі ПриватБанку
+*/
+    public Currency[] getPrivat(String currentdateDDpMMpYYYY) {
+        return new Currency[]{new Currency(840, "02.10.2023", 33, 32)};
     }
 }
