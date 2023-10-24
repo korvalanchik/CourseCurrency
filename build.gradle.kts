e1aa4b05faacaa0e18f9dc839a1edb83631fb793
korvalanchik <138583517+korvalanchik@users.noreply.github.com>
2023-10-24 21:50:07 +0300

    Bot begining (#5)
    * Ver. 3.0.5Beta
    
    * Ver. 4.0.1 (Add Bot)
    
    * Ver. 4.0.1 (Add Bot)
    
    * Ver. 4.0.1 (Add Bot)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
-    // https://mvnrepository.com/artifact/org.telegram/telegrambots
-    implementation("org.telegram:telegrambots:6.8.0")
     // https://mvnrepository.com/artifact/org.projectlombok/lombok
     compileOnly("org.projectlombok:lombok:1.18.30")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter
-    implementation("org.springframework.boot:spring-boot-starter:3.1.5")
-    // https://mvnrepository.com/artifact/org.telegram/telegrambots-spring-boot-starter
-    implementation("org.telegram:telegrambots-spring-boot-starter:6.8.0")
-
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
 }
 
 tasks.test {
