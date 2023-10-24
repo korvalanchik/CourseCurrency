899716ac328ae0cbbeeb15bc037b9528612899a7
korvalanchik <korvalan@yahoo.com>
2023-10-24 23:02:34 +0300

    Ver. 3.0.6 Bot

--- src/main/java/CurrencyBot.java
+++ src/main/java/CurrencyBot.java
@@ -1,54 +1,8 @@
-<<<<<<< HEAD
-import config.BotConfig;
-import currencyservice.CourseCurrency;
-=======
->>>>>>> ce84d83 (Bot begining (#5))
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-<<<<<<< HEAD
-import java.io.IOException;
-import java.util.Arrays;
-
-public class CurrencyBot extends TelegramLongPollingBot {
-    private String botToken = BotConfig.TOKEN;
-    private String botUsername = BotConfig.BOT_NAME;
-    @Override
-    public String getBotUsername() {
-        return botUsername;
-    }
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        CourseCurrency currencyModel = new CourseCurrency();
-        String currency = "";
-
-        if(update.hasMessage() && update.getMessage().hasText()){
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
-
-            switch (messageText){
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                default:
-                    try {
-                        currency = Arrays.toString(currencyModel.getNBU(""));
-
-                    } catch (IOException e) {
-                        sendMessage(chatId, "We have not found such a currency." + "\n" +
-                                "Enter the currency whose official exchange rate" + "\n" +
-                                "you want to know in relation to BYN." + "\n" +
-                                "For example: USD");
-                    }
-                    sendMessage(chatId, currency);
-=======
 public class CurrencyBot extends TelegramLongPollingBot {
 
     @Override
@@ -63,31 +17,11 @@ public class CurrencyBot extends TelegramLongPollingBot {
                 execute(message); // Call method to send the message
             } catch (TelegramApiException e) {
                 e.printStackTrace();
->>>>>>> ce84d83 (Bot begining (#5))
             }
         }
 
     }
 
-<<<<<<< HEAD
-    private void startCommandReceived(Long chatId, String name) {
-        String answer = "Hi, " + name + ", nice to meet you!" + "\n" +
-                "Enter the currency whose official exchange rate" + "\n" +
-                "you want to know in relation to BYN." + "\n" +
-                "For example: USD";
-        sendMessage(chatId, answer);
-    }
-
-    private void sendMessage(Long chatId, String textToSend){
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(String.valueOf(chatId));
-        sendMessage.setText(textToSend);
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-
-        }
-=======
     @Override
     public String getBotUsername() {
         return null;
@@ -96,6 +30,5 @@ public class CurrencyBot extends TelegramLongPollingBot {
     @Override
     public void onRegister() {
         super.onRegister();
->>>>>>> ce84d83 (Bot begining (#5))
     }
 }
