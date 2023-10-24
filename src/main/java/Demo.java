26d566ff43229087b26582ceb2170408fcafcbad
korvalanchik <korvalan@yahoo.com>
2023-10-24 19:36:03 +0300

    Ver. 4.0.1 (Add Bot)

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,10 +2,14 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TelegramApiException {
+        // You can use your own BotSession implementation if needed.
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
         try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new CurrencyBot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
