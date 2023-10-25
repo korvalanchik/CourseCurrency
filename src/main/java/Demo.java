c23875eb64052b44b55acbe901a30b82fb677be2
korvalanchik <korvalan@yahoo.com>
2023-10-25 18:12:39 +0300

    Ver. 4.0.2 (Add Bot)

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,3 +1,4 @@
+import CurrencyService.CurrencyBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
