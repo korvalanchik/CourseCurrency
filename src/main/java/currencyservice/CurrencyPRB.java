fa09c4e0cde2b92d37cffa0a1d592fc0162e447b
korvalanchik <korvalan@yahoo.com>
2023-10-25 20:57:09 +0300

    Ver. 4.0.2 (Add Bot)

---
+++ src/main/java/currencyservice/CurrencyPRB.java
@@ -0,0 +1,36 @@
+package currencyservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@JsonIgnoreProperties({"baseCurrency", "saleRateNB", "purchaseRateNB"})
+public class CurrencyPRB {
+    @JsonProperty("currency")
+    private String currency;
+    @JsonProperty("date")
+    private String date;
+    @JsonProperty("saleRate")
+    private float rateSell;
+    @JsonProperty("purchaseRate")
+    private float rateBuy;
+
+    @Override
+    public String toString() {
+        return "Rate{" +
+                "currency='" + currency + '\'' +
+                ", date='" + date + '\'' +
+                ", rateSell=" + rateSell +
+                ", rateBuy=" + rateBuy +
+                '}';
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
