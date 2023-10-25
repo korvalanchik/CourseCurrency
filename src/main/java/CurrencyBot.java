c0c539305a954c9168b12a04113bb9d3e9ab6c46
korvalanchik <korvalan@yahoo.com>
2023-10-25 20:57:09 +0300

    Ver. 4.0.2 (Add Bot)

---
+++ src/main/java/CurrencyBot.java
@@ -0,0 +1,81 @@
+import currencyservice.CourseCurrency;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Slf4j
+@Component
+@PropertySource("application.yml")
+public class CurrencyBot extends TelegramLongPollingBot {
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Value("${bot.username}")
+    private String botUsername;
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        CourseCurrency currencyModel = new CourseCurrency();
+        String currency = "";
+
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    try {
+                        currency = Arrays.toString(currencyModel.getNBU(""));
+
+                    } catch (IOException e) {
+                        sendMessage(chatId, "We have not found such a currency." + "\n" +
+                                "Enter the currency whose official exchange rate" + "\n" +
+                                "you want to know in relation to BYN." + "\n" +
+                                "For example: USD");
+                    }
+                    sendMessage(chatId, currency);
+            }
+        }
+
+    }
+
+    private void startCommandReceived(Long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!" + "\n" +
+                "Enter the currency whose official exchange rate" + "\n" +
+                "you want to know in relation to BYN." + "\n" +
+                "For example: USD";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
