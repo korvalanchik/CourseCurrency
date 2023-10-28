cd3f03b8ee479c0ef8ae065e5f6e408489840dfe
korvalanchik <korvalan@yahoo.com>
2023-10-28 03:51:58 +0300

    Ver. 4.0.4 (Add Bot)

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,7 +2,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-@Slf4j
+
 public class Demo {
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
