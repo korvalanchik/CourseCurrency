2c484ee01e918fa00549501f48b598c283284605
korvalanchik <138583517+korvalanchik@users.noreply.github.com>
2023-11-06 22:20:44 +0200

    Delete src/main/java/config/BotConfig.java

--- src/main/java/config/BotConfig.java
+++
@@ -1,6 +0,0 @@
-package config;
-
-public class BotConfig {
-    public static final String BOT_NAME = "HomeCurrencyBot";
-    public static final String TOKEN = "7771889610:AAEVchesaLLwRvecryHwk-MTOF-8PsWGpeU";
-}
